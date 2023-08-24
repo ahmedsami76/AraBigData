60b3249d66cbde211c475d40ded49faeb4367158
Hazem Omar <Hazematwwa@gmail.com>
2023-08-24 10:33:52 +0200

    Print added

--- Hadoop/2.MapReduce/src/main/python/max_temperature_map.py
+++ Hadoop/2.MapReduce/src/main/python/max_temperature_map.py
@@ -7,4 +7,5 @@ for line in sys.stdin:
   val = line.strip()
   (year, temp, q) = (val[15:19], val[87:92], val[92:93])
   if (temp != "+9999" and re.match("[01459]", q)):
-    print "%s\t%s" % (year, temp)
+    print ("%s\t%s" % (year, temp)
+)
